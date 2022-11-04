4fd1e09760e762f9d6099f1bb079aba9fe43c050
Khawlah Khalid <khawlah1920@hotmail.com>
2022-11-04 10:08:33 +0300

    winston package

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const winston = require('winston');
+
 
 const app = express();
 require('dotenv').config();
 const booksRoute = require('./Routes/books');
+
 
 const PORT = process.env.PORT || 3000
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+      new winston.transports.Console({
+        format:winston.format.combine(
+            winston.format.colorize({all:true})
+        )
+      }),
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    ],
+    exceptionHandlers: [
+      new winston.transports.File({ filename: 'exceptions.log' })
+    ]
+  });
+
 
 //Routes
 app.use('/api/books',booksRoute);
+throw new Error();
 
 
 //Connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL,
 {useNewUrlParser: true}).then(()=>{
-    console.log("Connect with mongodb atlas")
+    logger.info("Connected to mongodb atlas")
 }).catch(error=>{
-    console.log("Something Wrong Happened!",error);
+    logger.error(error.message);
 })
 
 
 
 app.listen(PORT,()=>{
-    console.log("Server started at PORT: ",PORT);
+    logger.info(`Server started at PORT: ${PORT}`);
 })
